0518aad856b148cdf92645844fb1df58a99bf535
Bryan Ngo <bryanngo@berkeley.edu>
2021-05-26 11:46:48 -0700

    fix todo list parsing

--- popup/blocked.js
+++ popup/blocked.js
@@ -1,42 +1,74 @@
-const hidePage = `body > :not(.blocked-msg) {
+'use strict';
+const hidePage = `html > :not(blocked-msg) {
                     display: none;
                   }`;
 
-import fs from "fs";
+const TODOS = "../data/todo.json";
+const BLOCKED_PGS = "../data/blocked-sites.json";
+const POPUP = "/popup/blocked.html";
 
-const TODOS = "/data/todo.json";
-const BLOCKED = "/data/blocked-sites.json";
-const todoData = JSON.parse(fs.readFileSync(TODOS));
+async function fetchJSON(path) {
+    try {
+        const response = await fetch(path);
+        return response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function fetchHTML(path) {
+    try {
+        const response = await fetch(path);
+        return response.text();
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 function insertAfter(newNode, existingNode) {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
 
 function displayTasks(tasks) {
-    const todoList = document.createElement("div");
-    for (const t in tasks) {
-        taskBox = document.createElement("input");
-        taskBox.append(t);
-        taskBox.setAttribute("type", "checkbox");
-        if (tasks[t]) {
-            taskBox.setAttribute("onClick", "return false;");
-            taskbox.setAttribute("checked", "true");
-        } else {
-            taskbox.setAttribute("checked", "false");
-        }
-        todoList.append(taskBox);
-    }
     const blockedMsg = document.getElementById("blocked-msg");
-    insertAfter(todoList, blockedMsg);
-}
+    for (const t in tasks) {
+        const todoList = document.createElement("div");
 
-function handleCreated(tab) {
-    const blockedData = JSON.parse(fs.readFileSync(BLOCKED));
-    if (blockedData.blockedSites.includes(tab.url)) {
-        browser.tabs.insertCSS({code: hidePage}).then(() => {
-            displayTasks(todoData);
-        });
+        const taskBox = document.createElement("input");
+        taskBox.type = "checkbox";
+        taskBox.disabled = tasks[t];
+        taskBox.checked = tasks[t];
+        taskBox.name = t;
+
+        const label = document.createElement("label");
+        label.for = t;
+        label.append(t);
+        todoList.append(taskBox);
+        todoList.append(label);
+
+        blockedMsg.insertBefore(todoList, null);
     }
 }
 
-browser.tabs.onCreated.addListener(handleCreated);
+async function handleCreated(_tabID, _changeID, tab) {
+    const blockedData = await fetchJSON(BLOCKED_PGS);
+    if (blockedData.blockedSites.includes(tab.url)) {
+        browser.tabs.insertCSS({code: hidePage});
+        const tasks = await fetchJSON(TODOS);
+        const newHTML = await fetchHTML(POPUP);
+        document.write(newHTML);
+        displayTasks(tasks);
+    }
+    browser.tabs.onUpdated.removeListener(handleCreated);
+}
+
+browser.tabs.onCreated.addListener(() => {
+    browser.tabs.onUpdated.addListener(handleCreated);
+});
+
+// async function blah() {
+//     const tasks = await fetchJSON(TODOS);
+//     displayTasks(tasks);
+// }
+
+// blah();
